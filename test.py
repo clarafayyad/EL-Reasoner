d197bfd542a3591b7ccf17162a8408be39b9db12
Clara Fayyad <clarafayad@live.com>
2024-12-04 22:32:37 +0100

    Adjust expectations

--- test.py
+++ test.py
@@ -3,20 +3,20 @@ from py4j.java_gateway import JavaGateway
 from el_reasoner import ELReasoner
 
 test_cases = {
-    # "1": {
-    #     "file": "animal.owl",
-    #     "class": "Dog",
-    #     "expected_subsumers": ["Dog", "Mammal", "Animal", "⊤"],
-    # },
-    # "2": {
-    #     "file": "transport.owl",
-    #     "class": "Bus",
-    #     "expected_subsumers": ["Bus", "PublicTransport", "Vehicle", "⊤", "∃hasFuelType.Diesel"],
-    # },
+    "1": {
+        "file": "animal.owl",
+        "class": "Dog",
+        "expected_subsumers": ["Dog", "Mammal", "Animal", "⊤"],
+    },
+    "2": {
+        "file": "transport.owl",
+        "class": "Bus",
+        "expected_subsumers": ["Bus", "PublicTransport", "Vehicle", "⊤", "∃hasFuelType.Diesel"],
+    },
     "3": {
         "file": "person.owl",
         "class": "Child",
-        "expected_subsumers": ["Child", "Person", "GrandChild", "∃hasParent.Child", "∃hasParent.Person"],
+        "expected_subsumers": ["Child", "Person", "∃hasParent.Person"],
     }
 }
 
@@ -28,7 +28,7 @@ for number, test in test_cases.items():
     formatter = gateway.getSimpleDLFormatter()
     reasoner = ELReasoner(ontology, formatter)
     subsumers = reasoner.get_all_subsumers(test["class"])
-    print(subsumers)
+    # print(subsumers)
     for expected_subsumer in test["expected_subsumers"]:
         if not expected_subsumer in subsumers:
             print("Expected subsumer " + expected_subsumer + " not found")
